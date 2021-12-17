07b48ed4e49303d0e34f9e305b60fc3bcc9664a4
lifeoncode <ndu918@outlook.com>
2021-12-17 22:03:15 +0200

    for in loop in javascript is awesome! and IIFEs are super weird!

--- web/main.js
+++ web/main.js
@@ -1,9 +1,19 @@
+setInterval(() => {
+    showTime();
+}, 1000)
 
+function showTime(){
+    document.body.style.padding = '100px';
+    document.body.style.textAlign = 'center';
+    document.body.innerHTML = `<h1>${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}</h1>`
+}
 
-let message = 'this is cool';
+const user = {
+    name: 'Josh',
+    surname: 'Mason',
+    age: 34
+}
 
-let list = message.split()
-
-console.log(list)
-
-
+for(let item in user){
+    console.log(`${item}: ${user[item]}`)
+}
