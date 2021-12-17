17dc8965d466d0b4e63bb4b97bab350ed421e8a5
lifeoncode <ndu918@outlook.com>
2021-12-17 10:07:59 +0200

    modern javascript from the beginning

---
+++ main.py
@@ -0,0 +1,41 @@
+import turtle
+from random import choice
+
+
+s = turtle.getscreen()
+
+# players
+player_one = turtle.Turtle()
+player_one.color('blue')
+player_one.shape('turtle')
+player_one.penup()
+player_one.goto(-200, 100)
+
+player_two = player_one.clone()
+player_two.color('red')
+player_two.penup()
+player_two.goto(-200, -100)
+
+
+# homes
+player_one.goto(200, 50)
+player_one.pendown()
+player_one.circle(50)
+player_one.penup()
+player_one.goto(-200, 100)
+
+player_two.goto(200, -150)
+player_two.pendown()
+player_two.circle(50)
+player_two.penup()
+player_two.goto(-200, -100)
+
+
+
+# gameplay
+die = [1, 2, 3, 4, 5, 6]
+while 
+
+    
+    
+turtle.exitonclick()
