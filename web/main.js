65de839b1701dee5faf0bcbcc24facbb67938bfa
lifeoncode <ndu918@outlook.com>
2021-12-18 19:49:58 +0200

    small task list app

--- web/main.js
+++ web/main.js
@@ -2,39 +2,101 @@ const form = document.querySelector('form');
 const input = form.querySelector('input');
 const list = document.querySelector('ul');
 const clear = document.querySelector('#clear');
+const search = document.querySelector('#search');
+
+
+search.addEventListener('keyup', searchTask);
+function searchTask(e){
+    let tasks = list.querySelectorAll('li');
+    
+    tasks.forEach(function(task){
+        let toMatch = search.value.toLowerCase();
+        let theTask = task.textContent.toLowerCase();
+        if(theTask.indexOf(toMatch) !== -1){
+            task.style.display = 'flex';
+
+        }else{
+            task.style.display = 'none';
+        }
+    })
+}
 
 
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', saveTask);
+// save to database
+function saveTask(e){
     e.preventDefault();
+    const task = input.value;
+    if(localStorage.getItem('tasks')){
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        reload();
 
-    let li = document.createElement('li');
-    let remove = document.createElement('i')
-    li.textContent = input.value;
-    remove.setAttribute('class', 'lni lni-close');
-    input.value = null;
-    li.appendChild(remove);
-    list.appendChild(li);
-})
-
-
-clear.addEventListener('click', function(e) {
-    let items = list.children;
-    for(let x = 0; x <= items.length; x++){
-        console.log(items[x])
-    }
-})
-
-document.body.addEventListener('click', remove);
-
-
-function remove(e){
-    if(e.target.classList.contains('lni')){
-        let del = e.target;
-        let item = del.parentElement;
-        list.removeChild(item);
-        
     }else{
-        console.log('clicked on body')
+        const tasks = [];
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        reload();
     }
+}
+
+
+// pull from database to UI
+function fetchTasks(){
+    if(localStorage.getItem('tasks')){
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.forEach(task => {
+            let li = document.createElement('li');
+            let del = document.createElement('i');
+            li.textContent = task;
+            del.setAttribute('class', 'lni lni-close');
+            li.appendChild(del);
+            list.appendChild(li);
+        })
+    }
+}
+fetchTasks();
+
+
+
+// delete individual task
+document.body.addEventListener('click', deleteTask);
+function deleteTask(e){
+    if(e.target.classList.contains('lni')){
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        let toDelete = e.target.parentElement.textContent;
+        tasks.forEach(task => {
+            if(task === toDelete){
+                if(confirm('Are you sure?')){
+                    tasks.splice(tasks.indexOf(task), 1);
+                    localStorage.setItem('tasks', JSON.stringify(tasks));
+                    reload();
+                }
+            }
+        })
+    }
+}
+
+
+
+// delete all tasks
+clear.addEventListener('click', deleteAllTasks);
+function deleteAllTasks(e){
+    if(localStorage.getItem('tasks')){
+        if(confirm('All tasks will be deleted, continue?')){
+            localStorage.removeItem('tasks');
+            reload();
+        }
+    }
+}
+
+
+
+
+
+// reload tab
+function reload(){
+    window.location.reload();
 }
