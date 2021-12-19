51aacd3ab875b4cf7dc166b03a7a9fa912ae55e6
lifeoncode <ndu918@outlook.com>
2021-12-19 15:23:58 +0200

    number guessing game

--- web/main.js
+++ web/main.js
@@ -1,12 +1,43 @@
-const form = document.querySelector('form');
-const amount = form.querySelector('#amount');
-const interest = form.querySelector('#interest');
-const years = form.querySelector('#years');
-const monthlyPay = form.querySelector('#monthly-pay');
-const totalPay = form.querySelector('#total-pay');
-const totalInterest = form.querySelector('#total-interest');
+const form = document.querySelector('form'),
+    input = form.querySelector('input'),
+    error = document.querySelector('.error'),
+    success = document.querySelector('.success');
 
-form.addEventListener('submit', calculate);
-function calculate(e){
+
+let magicNum = Math.round(Math.random() * 10 + 1),
+    chances = 3;
+
+console.log(magicNum)
+
+form.addEventListener('submit', validate);
+function validate(e){
     e.preventDefault();
+
+    if(isNaN(Number(input.value)) || Number(input.value) < 1 || Number(input.value) > 10){
+        error.textContent = 'enter a number between 1 and 10';
+        setTimeout(() => {
+            error.textContent = '';
+            input.value = '';
+        }, 2000);
+        
+    }else if(Number(input.value) !== magicNum){
+        chances -= 1
+        error.textContent = `Wrong, you have ${chances} chances left.`;
+        setTimeout(() => {
+            error.textContent = '';
+            input.value = '';
+        }, 2000);
+        
+    }else{
+        success.textContent = 'Correct! you did it.'
+        chances = 3;
+        setTimeout(() => {
+            success.textContent = '';
+            input.value = '';
+        }, 3000)
+    }
+
+    if(chances === 0){
+        form.innerHTML = `GAME OVER!<br>number was: ${magicNum}`
+    }
 }
