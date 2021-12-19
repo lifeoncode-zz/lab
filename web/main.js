3110e21c92656aa8bdb7f5a59883201f7ecfce49
lifeoncode <ndu918@outlook.com>
2021-12-19 19:41:12 +0200

    constructor inheritence is powerful

--- web/main.js
+++ web/main.js
@@ -1,4 +1,30 @@
 
-const cal = new Function('x', 'y', 'return x + y');
 
-console.log(cal(1, 5))
+function Person(name, surname){
+    this.name = name;
+    this.surname = surname;
+}
+
+Person.prototype.greet = function(message){
+    return `${this.name} says "${message}"`;
+}
+
+const j = new Person('Jeremy', 'Clarkson');
+const b = new Person('Blake', 'Lewis');
+
+function Client(name, surname, id, membership){
+    // inherit from Person constructor
+    Person.call(this, name, surname);
+    
+    this.id = id;
+    this.membership = membership;
+}
+
+Client.prototype.info = function(){
+    return `${this.name} has been a member since ${this.membership}`
+}
+
+const m = new Client('Maise', 'Johnson', '44f', '2020');
+console.log(j.greet('Hello'))
+console.log(m.info())
+
