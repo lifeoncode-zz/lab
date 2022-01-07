e55ef428fdc2b33e945185a2aca5e599d8af04a8
lifeoncode <ndu918@outlook.com>
2022-01-07 22:35:38 +0200

    feat: calculator

---
+++ web/project_calculator/main.js
@@ -0,0 +1,26 @@
+// const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        switch(btn.textContent){
+            case '=':   try{
+                            display.textContent = eval(display.textContent);
+                        }catch{
+                            display.textContent = 'Invalid expression!';
+                        }
+                break;
+            
+            case 'c':   display.textContent = '';
+                break;
+
+            case '←':   display.textContent = display.textContent.slice(0, -1);
+                break;
+
+            default:    display.textContent += btn.textContent;
+
+        }
+    })
+
+})
+
