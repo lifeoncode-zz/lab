7bcb57345617d9e7e89a788757918cf97b4f768d
lifeoncode <ndu918@outlook.com>
2022-01-07 16:00:33 +0200

    working with json files, fetching and displaying data

---
+++ web/async/app.js
@@ -0,0 +1,42 @@
+const btn1 = document.querySelector('#btn1'),
+    btn2 = document.querySelector('#btn2');
+
+btn1.addEventListener('click', loadCustomer);
+
+function loadCustomer(){
+    const xhr = new XMLHttpRequest;
+    xhr.open('get', 'customer.json', true);
+
+    xhr.onload = function(){
+        if(this.status === 200){
+            const customer = JSON.parse(this.responseText);
+            document.querySelector('#customer').innerHTML = `
+            <ul>
+                <li><strong>ID:</strong> ${customer.name}</li>
+                <li><strong>Name:</strong> ${customer.name}</li>
+                <li><strong>Company:</strong> ${customer.company}</li>
+                <li><strong>Email:</strong> ${customer.email}</li>
+                <li><strong>Phone:</strong> ${customer.phone}</li>
+            </ul>
+            `;
+        }
+    }
+    
+    xhr.send();
+}
+
+
+btn2.addEventListener('click', loadCustomers);
+
+function loadCustomers(){
+    const xhr = new XMLHttpRequest;
+    xhr.open('get', 'customers.json', true);
+
+    xhr.onload = function(){
+        if(this.status === 200){
+            console.log(this.responseText);
+        }
+    }
+    
+    xhr.send();
+}
