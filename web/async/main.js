4147ad9c293c65f0e1f73e7c58eb7dac67dea447
lifeoncode <ndu918@outlook.com>
2022-01-07 15:20:19 +0200

    javascript asynchrounous programming

---
+++ web/async/main.js
@@ -0,0 +1,45 @@
+
+// few things to keep in mind
+
+// READY STATE VALUES
+// 0: request not initialized
+// 1: server connection established
+// 2: request received
+// 3: processing request
+// 4: request finished and response is ready
+
+// HTTP STATUSES
+// 200: "ok"
+// 403: "forbidden"
+// 404: "not found"
+
+
+
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', loadData);
+
+function loadData(e){
+    // create XHR Object
+    const xhr = new XMLHttpRequest();
+
+    // open
+    xhr.open('GET', 'data.txt', true);
+
+    xhr.onload = function(){
+        // check net status
+        if(this.status === 200){
+            console.log(this.responseText);
+            
+        }else if(this.status === 404){
+            document.querySelector('.container').innerHTML = `
+            <h1>Oops!</h1>
+            <p>Looks like we couldn't find what you're looking for.</p>
+            <a href="./index.html">Okay</a>
+            `
+        }
+    }
+
+    // send
+    xhr.send();
+}
