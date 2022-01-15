0f582522280b404896b3417d88a3d3fa33b8238d
lifeoncode <ndu918@outlook.com>
2022-01-15 07:11:33 +0200

    json db demo

--- python/main.py
+++ python/main.py
@@ -1,41 +1,33 @@
 import turtle
-from random import choice
+
+anton = turtle.Turtle()
+
+def draw_square():
+    anton.color('purple', 'yellow')
+    anton.begin_fill()
+
+    for i in range(4):
+        anton.fd(100)
+        anton.rt(90)
+
+    anton.end_fill()
+
+# one
+draw_square()
+
+anton.penup()
+anton.fd(150)
+anton.pendown()
+
+# two
+draw_square()
+
+anton.penup()
+anton.bk(300)
+anton.pendown()
+
+# three
+draw_square()
 
 
-s = turtle.getscreen()
-
-# players
-player_one = turtle.Turtle()
-player_one.color('blue')
-player_one.shape('turtle')
-player_one.penup()
-player_one.goto(-200, 100)
-
-player_two = player_one.clone()
-player_two.color('red')
-player_two.penup()
-player_two.goto(-200, -100)
-
-
-# homes
-player_one.goto(200, 50)
-player_one.pendown()
-player_one.circle(50)
-player_one.penup()
-player_one.goto(-200, 100)
-
-player_two.goto(200, -150)
-player_two.pendown()
-player_two.circle(50)
-player_two.penup()
-player_two.goto(-200, -100)
-
-
-
-# gameplay
-die = [1, 2, 3, 4, 5, 6]
-# while 
-
-    
-    
 turtle.exitonclick()
---
+++ web/shop/admin.js
@@ -0,0 +1,28 @@
+
+// const express = require('express');
+// const app = express();
+
+// const fs = require('fs');
+
+// const products = [
+//     {
+//         name: 'mouse',
+//         description: 'this is a mouse that is ergo',
+//         price: 420
+//     },
+
+//     {
+//         name: 'monitor',
+//         description: 'this is a monitor for pc',
+//         price: 1520,
+//     }
+// ]
+
+// saveData(products)
+
+
+
+// function saveData(products){
+//     const jsonFormat = JSON.stringify(products);
+//     console.log(jsonFormat)
+// }
