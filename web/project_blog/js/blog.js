d08e62b7b3b538de862b7bcc438c24d476f3a53a
lifeoncode <ndu918@outlook.com>
2022-04-16 23:42:40 +0200

    fix: image url

--- web/project_blog/js/blog.js
+++ web/project_blog/js/blog.js
@@ -22,18 +22,22 @@ function displayBlog(blog) {
     const blogContainer = document.createElement('div');
     const coverContainer = document.createElement('div');
     const textContainer = document.createElement('div');
+    const image = document.createElement('img');
     // assign classes
     blogContainer.className = 'blog';
     coverContainer.className = 'image';
     textContainer.className = 'text';
+    image.setAttribute('src', blog.cover);
     // content elements on-the-fly
-    coverContainer.innerHTML = `<img href="${blog.cover}" alt="blog cover image">`;
+    // coverContainer.innerHTML = `<img href="${blog.cover}" alt="blog cover image">`;
+    
     textContainer.innerHTML += `
     <h3 class="title">${blog.title}</h3>
     <p>${blog.paragraph}</p>
     <a href="#" class="underlined-link">Read more <span></span></a>
     `;
     // append elements
+    coverContainer.appendChild(image);
     blogContainer.appendChild(coverContainer);
     blogContainer.appendChild(textContainer);
 
