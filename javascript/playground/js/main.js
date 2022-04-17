430c095cdd4e93df1141f74352d86d13845d65b5
lifeoncode <ndu918@outlook.com>
2022-04-17 13:05:25 +0200

    dealing with event bubbling

---
+++ javascript/playground/js/main.js
@@ -0,0 +1,5 @@
+
+window.onclick = function() {console.log('window clicked')}
+document.body.onclick = function() {console.log('body clicked')}
+document.querySelector('.container').onclick = function() {console.log('container clicked')}
+document.querySelector('button').onclick = function() {console.log('button clicked')}
