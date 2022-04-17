6b4aab0e4b8b3440cde0e8c18c92f66568447b14
lifeoncode <ndu918@outlook.com>
2022-04-17 15:09:54 +0200

    we can access vars on other js scritpts - as long as html loads them first

--- javascript/playground/js/main.js
+++ javascript/playground/js/main.js
@@ -1,15 +1,2 @@
 
-window.onclick = function() {console.log('window clicked')}
-document.body.onclick = function() {console.log('body clicked')}
-document.querySelector('.container').onclick = function() {console.log('container clicked')}
-
-document.querySelector('button').addEventListener('click', function(e) {
-    e.stopPropagation();
-    console.log('button clicked')
-})
-
-
-window.addEventListener('contextmenu', function(e) {
-    e.preventDefault()
-    console.log('nah man...')
-})
+const str = "this is string"
