45f910336bfb9a6c5104c898f4e9a9a9b8c01f3b
lifeoncode <ndu918@outlook.com>
2022-04-17 13:39:03 +0200

    stop bubbling and default behaviour - take control

--- javascript/playground/js/main.js
+++ javascript/playground/js/main.js
@@ -2,4 +2,14 @@
 window.onclick = function() {console.log('window clicked')}
 document.body.onclick = function() {console.log('body clicked')}
 document.querySelector('.container').onclick = function() {console.log('container clicked')}
-document.querySelector('button').onclick = function() {console.log('button clicked')}
+
+document.querySelector('button').addEventListener('click', function(e) {
+    e.stopPropagation();
+    console.log('button clicked')
+})
+
+
+window.addEventListener('contextmenu', function(e) {
+    e.preventDefault()
+    console.log('nah man...')
+})
