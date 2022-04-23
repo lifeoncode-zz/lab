4f8b3d9d035d85f9feb10a1abc290771820fd653
lifeoncode <ndu918@outlook.com>
2022-04-23 22:22:27 +0200

    commenting

--- scripting/internet.py
+++ scripting/internet.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+# this small script checks if computer is connected
+# to the web by pinging google, if no response
+# then there must be no connection to internet, otherwise...
+
 from time import sleep
 import os
+
 
 def ping():
 	os.system("ping google.com -c 3 > ping_results.txt")
