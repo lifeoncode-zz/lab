8c85cc00cb4fc3da983e1dafe0dedd63d6fd0391
lifeoncode <ndu918@outlook.com>
2022-04-24 15:16:21 +0200

    handling or declaring multidim arrays with curly bracket method technique is better

--- java/playground/src/main/java/me/lifeoncode/App.java
+++ java/playground/src/main/java/me/lifeoncode/App.java
@@ -4,14 +4,7 @@ import java.util.Arrays;
 
 public class App {
     public static void main( String[] args ) {
-        int[][][] numbers = new int[2][3][3];
-        numbers[0][0][0] = 6;
-        numbers[0][0][1] = 55;
-        numbers[0][0][2] = 44;
-        System.out.println(Arrays.deepToString(numbers));
-        numbers[1][1][0] = 9;
-        numbers[1][1][1] = 41;
-        numbers[1][1][2] = 67;
+        int[][] numbers = { {4, 22, 6}, {55, 8, 9} };
         System.out.println(Arrays.deepToString(numbers));
     }
 }
