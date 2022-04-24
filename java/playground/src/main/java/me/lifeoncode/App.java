f8b0112d14a743a5e3a922fcceab9bcc36f89786
lifeoncode <ndu918@outlook.com>
2022-04-24 15:09:57 +0200

    yeah, multidimensional arrays can get pretty crazy

--- java/playground/src/main/java/me/lifeoncode/App.java
+++ java/playground/src/main/java/me/lifeoncode/App.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 
 public class App {
     public static void main( String[] args ) {
-        int[][] numbers = new int[2][3];
-        numbers[0][0] = 6;
-        numbers[0][1] = 55;
-        numbers[0][2] = 44;
+        int[][][] numbers = new int[2][3][3];
+        numbers[0][0][0] = 6;
+        numbers[0][0][1] = 55;
+        numbers[0][0][2] = 44;
         System.out.println(Arrays.deepToString(numbers));
-        numbers[1][0] = 9;
-        numbers[1][1] = 41;
-        numbers[1][2] = 67;
+        numbers[1][1][0] = 9;
+        numbers[1][1][1] = 41;
+        numbers[1][1][2] = 67;
         System.out.println(Arrays.deepToString(numbers));
     }
 }
