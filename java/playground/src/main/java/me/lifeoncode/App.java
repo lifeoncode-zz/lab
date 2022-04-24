3caa0bac9a15cfce32e90a88c6483f13a6723c49
lifeoncode <ndu918@outlook.com>
2022-04-24 20:29:22 +0200

    fizzbuzz with ternary

--- java/playground/src/main/java/me/lifeoncode/App.java
+++ java/playground/src/main/java/me/lifeoncode/App.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class App {
     public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
-        print(true, "enter salary: ");
-        int income = scanner.nextInt();
-        boolean hasHighIncome = income > 60_000;
-        String message = hasHighIncome ? "Upper class" : "Middle class";
+        print(true, "enter number: ");
+        int num = scanner.nextInt();
+
+        boolean three = (num % 3 == 0);
+        boolean five = (num % 5 == 0);
+
+        String message = (three && five) ? "FizzBuzz" : (five && !three) ? "Fizz" :
+                (!five && three) ? "Buzz" : String.format("%s",num);
         print(false, message);
     }
+
 
     static void print(boolean sameLine, String text) {
         if (sameLine) {
