4c71cc508cbd5d83c392f48b689960c5ef643974
lifeoncode <ndu918@outlook.com>
2022-04-24 20:51:16 +0200

    while(true) is apparently professional

--- java/playground/src/main/java/me/lifeoncode/App.java
+++ java/playground/src/main/java/me/lifeoncode/App.java
@@ -3,26 +3,20 @@ package me.lifeoncode;
 import java.util.Scanner;
 
 public class App {
+
     public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
-        print(true, "enter number: ");
-        int num = scanner.nextInt();
 
-        boolean three = (num % 3 == 0);
-        boolean five = (num % 5 == 0);
-
-        String message = (three && five) ? "FizzBuzz" : (five && !three) ? "Fizz" :
-                (!five && three) ? "Buzz" : String.format("%s",num);
-        print(false, message);
-    }
-
-
-    static void print(boolean sameLine, String text) {
-        if (sameLine) {
-            System.out.print(text);
-        } else {
-            System.out.println(text);
+        while(true) {
+            System.out.print("enter something: ");
+            String user = scanner.nextLine();
+            if (user.equalsIgnoreCase("quit")) {
+                break;
+            }
+            System.out.println("you said: "+user);
         }
+
     }
+
 }
 
