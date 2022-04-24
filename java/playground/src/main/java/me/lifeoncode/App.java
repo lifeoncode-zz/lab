e29b5a09d51a5bddb0ada67880c63bb04e5af5c7
lifeoncode <ndu918@outlook.com>
2022-04-24 20:08:01 +0200

    middle and upper class program with ternary ternary

--- java/playground/src/main/java/me/lifeoncode/App.java
+++ java/playground/src/main/java/me/lifeoncode/App.java
@@ -1,22 +1,23 @@
 package me.lifeoncode;
 
-import org.w3c.dom.ls.LSOutput;
-
-import java.text.FieldPosition;
-import java.text.NumberFormat;
-import java.text.ParsePosition;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main( String[] args ) {
-        int income = 120_000;
-        String classLevel = (income > 60_000) ? "First" : "Economy";
-        print(classLevel);
+        Scanner scanner = new Scanner(System.in);
+        print(true, "enter salary: ");
+        int income = scanner.nextInt();
+        boolean hasHighIncome = income > 60_000;
+        String message = hasHighIncome ? "Upper class" : "Middle class";
+        print(false, message);
     }
 
-    static void print(String text) {
-        System.out.println(text);
+    static void print(boolean sameLine, String text) {
+        if (sameLine) {
+            System.out.print(text);
+        } else {
+            System.out.println(text);
+        }
     }
 }
 
