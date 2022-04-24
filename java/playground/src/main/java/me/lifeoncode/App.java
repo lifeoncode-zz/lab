5b0ca48af8ecdd02752fa84be6071fe4a76f79f4
lifeoncode <ndu918@outlook.com>
2022-04-24 17:24:50 +0200

    formatting numbers to local currency

--- java/playground/src/main/java/me/lifeoncode/App.java
+++ java/playground/src/main/java/me/lifeoncode/App.java
@@ -1,20 +1,16 @@
 package me.lifeoncode;
 
+import java.text.FieldPosition;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main( String[] args ) {
-        Scanner scanner = new Scanner(System.in);
-        String n = scanner.nextLine();
-        try {
-            int num = Integer.parseInt(n);
-            System.out.println("success");
-
-        } catch (Exception err) {
-            System.out.println("failed to convert: "+err.getMessage());
-        }
-
-        System.out.println("\nProgram ran to completion...");
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String salary = currency.format(64888);
+        System.out.println(salary);
     }
 }
+
