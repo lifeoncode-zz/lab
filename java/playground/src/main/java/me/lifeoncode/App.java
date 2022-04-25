0f4a1071aebed898cd54532e2c0378ea9c3b8f11
fzondi021 <fzondi021@student.wethinkcode.co.za>
2022-04-25 16:36:38 +0200

    Building bank

--- java/playground/src/main/java/me/lifeoncode/App.java
+++ java/playground/src/main/java/me/lifeoncode/App.java
@@ -1,18 +1,29 @@
 package me.lifeoncode;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
 
     public static void main( String[] args ) {
-        String[] items = {"milk", "juice", "coffee beans", "sugar", "mango"};
-        for (String item: items) {
-            System.out.println(item);
+        Bank jake = new Bank("Jake Tran", "jake@jaketran.io", "0728234256");
+        String phone = jake.getPhone();
+        ArrayList <String> accountNum = new ArrayList<String>();
+        Random random = new Random();
+
+        for (int i = 0; i < phone.length(); i++) {
+            String randomNum = String.format("%s", random.nextInt(10));
+            accountNum.add(randomNum);
         }
-        System.out.println("\nthen...\n");
-        for (int i = items.length-1; i >= 0; i--) {
-            System.out.println(items[i]);
+        String account = "";
+        for (String num: accountNum) {
+            account += num;
         }
+
+        System.out.println(account);
     }
 
 }
--- java/playground/src/main/java/me/lifeoncode/Bank.java
+++ java/playground/src/main/java/me/lifeoncode/Bank.java
@@ -1,11 +1,57 @@
 package me.lifeoncode;
 
-public class Bank {
-    private final int balance;
-    private final String name;
+import java.util.ArrayList;
+import java.util.Random;
 
-    public Bank(int balance, String name) {
-        this.balance = balance;
+public class Bank {
+    private String account;
+    private double balance = 0.00;
+    private final String name;
+    private final String email;
+    private final String phone;
+    private Random random;
+
+    public Bank(String name, String email, String phone) {
         this.name = name;
+        this.email = email;
+        this.phone = phone;
+        ArrayList<String> accountNums = new ArrayList<String>();
+
+    }
+
+    public String getAccount() {
+        return this.account;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+            System.out.println("deposit successful: +"+amount);
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            System.out.println("successful withdrawal: -"+amount);
+        } else {
+            System.out.println("unsuccessful withdrawal: insufficient funds");
+        }
     }
 }
