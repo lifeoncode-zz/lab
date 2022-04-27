088e3d55e4b7ff0d430e87530843c6ce7e779200
lifeoncode <ndu918@outlook.com>
2022-04-27 21:31:47 +0200

    Statis methods cannot access class instance vars and instance methods directly, Instance methods have direct access to static vars and static methods.

--- java/playground/src/main/java/me/lifeoncode/App.java
+++ java/playground/src/main/java/me/lifeoncode/App.java
@@ -1,19 +1,27 @@
 package me.lifeoncode;
 
 class Calculator {
+    private int num;
+    private static int AB;
+
     public static void add(int x, int y) {
         System.out.printf("%s + %s = %s\n", x, y, (x+y));
+        System.out.println(AB);
+//        System.out.println(num);
+//        subtract()
     }
 
     public void subtract(int x, int y) {
         System.out.printf("%s - %s = %s\n", x, y, (x-y));
+        add(x, y);
+        System.out.println(num);
+        System.out.println(AB);
     }
 }
 
 public class App {
 
     public static void main( String[] args ) {
-        Calculator.add(4, 8);
         Calculator calculator = new Calculator();
         calculator.subtract(10, 4);
     }
