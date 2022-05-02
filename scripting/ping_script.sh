0df1e6bfbe39f7263f4b674431b5218df9814a40
lifeoncode <ndu918@outlook.com>
2022-05-02 16:57:07 +0100

    local network ping script

---
+++ scripting/ping_script.sh
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env bash
+
+# ifconfig > network.txt
+# # for i in {0..10}
+# # do
+# grep "inet " network.txt > file.txt
+
+# more file.txt
+
+# # done
+
+echo "dig" > filename.txt
+cat filename.txt
+sed -i "s/di/" filename.txt
+cat filename.txt
