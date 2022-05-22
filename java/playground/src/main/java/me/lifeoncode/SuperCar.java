ab46953652130b39657f748612add51641273428
lifeoncode <ndu918@outlook.com>
2022-05-22 21:21:34 +0200

    I dont know

--- java/playground/src/main/java/me/lifeoncode/SuperCar.java
+++ java/playground/src/main/java/me/lifeoncode/SuperCar.java
@@ -1,2 +1,11 @@
-package me.lifeoncode;public class SuperCar {
+package me.lifeoncode;
+
+public class SuperCar extends Car {
+    public SuperCar(String manufacturer, String model, int year) {
+        this("v8", manufacturer, year, 4, 2, model, 7, 320);
+    }
+
+    public SuperCar(String engine, String manufacturer, int year, int wheels, int doors, String model, int maxGear, int topSpeed) {
+        super(engine, manufacturer, year, wheels, doors, model, maxGear, topSpeed);
+    }
 }
